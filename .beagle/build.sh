9b020fa7615c9a9fea8e38a71bfbd50e979770c1
shucheng <shucheng@bd-apaas.com>
2024-10-15 15:50:18 +0800

    update noVNC to upstream/main

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -7,10 +7,11 @@ git config --global --add safe.directory $PWD
 npm config set registry https://registry.npmmirror.com
 
 name=noVNC
-version=v1.5.0
+# version=v1.5.0
 if [ ! -d "$PWD/src/assets/noVNC" ]; then
   git config --global http.proxy 'socks5://www.ali.wodcloud.com:1283'
-  git clone --recurse-submodules -b ${version} https://github.com/novnc/noVNC.git $PWD/src/assets/noVNC
+  # git clone --recurse-submodules -b ${version} https://github.com/novnc/noVNC.git $PWD/src/assets/noVNC
+  git clone --recurse-submodules https://github.com/novnc/noVNC.git $PWD/src/assets/noVNC
 fi
 
 # git apply .beagle/1.4.1-i18n-angular.patch
